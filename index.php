355ee55a1a2824ec9bedb14c32131b811ceca4aa
KodingCore <kcorvais@gmail.com>
2023-06-28 12:14:18 +0000

    calc fin

--- index.php
+++ index.php
@@ -1,29 +1,46 @@
 <?php
 
-require "calculator.phtml";
 require "operations.php";
 
 
 if(isset($_POST["nbr_one"]) && !empty($_POST["nbr_one"]) && 
 isset($_POST["nbr_two"]) && !empty($_POST["nbr_two"]) && 
 isset($_POST["operator"]) && !empty($_POST["operator"])){
+    
     if($_POST["operator"] === "+"){
         
         $result = add($_POST["nbr_one"], $_POST["nbr_two"]);
-    }else if($_POST["operator"] == "-"){
+        
+    }else if($_POST["operator"] === "-"){
 
         $result = substract($_POST["nbr_one"], $_POST["nbr_two"]);
-    }else if($_POST["operator"] == "X"){
+        
+    }else if($_POST["operator"] === "X"){
 
         $result = multiply($_POST["nbr_one"], $_POST["nbr_two"]);
-        var_dump($result);
-    }else if($_POST["operator"] == "/"){
+        
+    }else if($_POST["operator"] === "/"){
 
         $result = divide($_POST["nbr_one"], $_POST["nbr_two"]);
-    }else if($_POST["operator"] == "%"){
+        
+    }else if($_POST["operator"] === "%"){
 
         $result = modulo($_POST["nbr_one"], $_POST["nbr_two"]);
+        
+    }else if($_POST["operator"] === "^"){
+
+        $result = power($_POST["nbr_one"], $_POST["nbr_two"]);
+        
     }
+    require "calculator.phtml";
+}else if(isset($_POST["nbr_one"]) && !empty($_POST["nbr_one"]) && isset($_POST["operator"]) && !empty($_POST["operator"])){
+    if($_POST["operator"] === "!"){
     
+        $result = factorial($_POST["nbr_one"]);
+        
+    }
+    require "calculator.phtml";
+}else{
+    require "calculator.phtml";
 }
 ?>
--- operations.php
+++ operations.php
@@ -41,4 +41,28 @@ function modulo(int $nb1, int $nb2) : ? int
     
 }
 
+function power(int $nb1, int $nb2) : int
+{
+    if($nb2 === 0){
+        return null;
+    }else{
+        $res = pow($nb1, $nb2);
+        return $res;
+    }
+}
+
+function factorial(int $nb1) : int
+{
+    if($nb1 === 0){
+        return 1;
+    }else{
+        $res = 1;
+        for($i=1; $i <= $nb1; $i++){
+            $res = $res * $i;
+        }
+        return $res;
+    }
+    
+}
+
 ?>
